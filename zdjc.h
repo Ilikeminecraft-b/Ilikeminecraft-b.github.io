451f4e2abe67d6cbb5737f53c008d4aaaf2b5ff4
Ilikeminecraft-b <118666653+Ilikeminecraft-b@users.noreply.github.com>
2022-11-21 20:44:21 +0800

    Add files via upload

---
+++ zdjc.h
@@ -0,0 +1,53 @@
+//Version 1.0
+#define cot cout
+#define cut cout
+#define cotu cout
+#define cni cin
+#define ci cin
+#define cn cin
+#define namespce namespace
+#define namespaec namespace
+#define naemspce namespace
+#define esle esle
+#define eles esle
+#define sels else
+#define retunr return
+#define retun return
+#define True true
+#define ture true
+#define flase false
+#define fales false
+#define fro for
+#define fr for
+#define enld endl
+#define eldl endl
+#define doube double
+#define doubel double
+#define itn int
+#define wihle while
+#define wilel while
+//这是一个自动纠错头文件 
+//使用前将它复制到 C:\Program Files (x86)\Dev-Cpp\MinGW64\lib\gcc\x86_64-w64-mingw32\4.9.2\include\c++中
+//使用时加上"#include<zdjc.h>即可" 
+/*
+自动纠错范围：
+cin:cni,ci,cn;
+cout:cot,cotu,cut;
+namespace:namespce,namespaec,naemspce;
+if:fi;
+else:esle,eles,sels;
+return:retunr,retun;
+true:ture,True;
+false:flase,fales;
+for:fro,fr;
+endl:enld,eldl;
+double:doubl,doubel
+int:itn;
+while:wihle,wilel;
+
+*/
+/*
+作者：Ilikeminecraft
+QQ:2330198453 
+主页：http://topurl.cn/toKen
+*/ 
